234e543c9a2c246946424c5767c2d3667c66bb2c
bohda634 <bohda634@student.otago.ac.nz>
2023-05-08 17:14:20 +1200

    added testing and output csv file

--- countingItUp.java
+++ countingItUp.java
@@ -1,10 +1,46 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 class countingItUp {
 
     static long nuumCount = 0;
 
-    public static void main(String[] args) {
-        System.out.print(solve(3, 2));
+    public static void main(String[] args) throws Exception {
+        
+        FileWriter f = new FileWriter("/Users/danielbohinc/Documents/2023/COSC326/etude6-countingitup/testOutput.csv");
+
+        f.write("Numerator, Denominator, Average Time, Output\n");
+        runTests(f);
+        System.out.println(solve(11, 2));
+
+    }
+
+    private static void runTests(FileWriter f) throws Exception {
+
+        long endTime = 0;
+        long answer = 0;
+        long startTime = 0;
+        long average = 0;
+
+        for(int i = 1; i < 53; i++) {
+            for (int denom = 1; denom < 11; denom++) {
+                for (int j = 0; j < 10; j++) {
+                    startTime = System.nanoTime();
+                    answer = solve(i, denom);
+                    endTime = System.nanoTime();
+                    average += endTime - startTime;
+                }
+                average = average / 10;
+                try {
+                    f.write(i + ", " + denom + ", " + Long.toString(average) + ", " + Long.toString(answer) + "\n");
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+        f.close();
     }
 
     /**
@@ -35,6 +71,8 @@ class countingItUp {
     }
 
     public static long solve(long x, long y) {
-        return factorialNumerator(x, y) / factorialDenom(y);
+        long value = factorialNumerator(x, y) / factorialDenom(y);
+        nuumCount = 0;
+        return value;
     }
 }
