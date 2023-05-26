61752d3e281970b55d37ca7bbd5eb4171d831b9a
Ben Knox <booknox10@gmail.com>
2023-05-26 17:21:17 +1200

    V5 may pass the judge, not sure

--- CountingItUpV5.java
+++ CountingItUpV5.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
 public class CountingItUpV5 {
-    
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
-            // denomList = new ArrayList<Long>();
+
             String input = sc.nextLine();
             input.strip();
             String[] inputArr = input.split(" ");
@@ -22,17 +21,33 @@ public class CountingItUpV5 {
     }
 
     public static long binomial(long n, long k) {
-        long result = 1, i;
-      
-        if (k > n-k) // take advantage of symmetry
-            k = n-k;
-      
-        for (i = 1; i <= k; i++, n--) {
-            if (result/i > Long.MAX_VALUE/n) // return 0 on potential overflow
-                return 0;
+        long result = 1;
 
-            result = result / i * n + result % i * n / i; // split result * n / i into (result / i * i + c % i) * n / i
+        long divResult;
+        long divRemainder;
+        long multiply1;
+        long multiply2;
+
+        k = Math.min(k, n - k);
+
+        for (long i = 1; i <= k; i++) {
+            /**
+             * might not need line below because we wont be testing values which will
+             * overflow I'd hope
+             */
+            // if (result / i > Long.MAX_VALUE / n)
+            // return 0;
+
+            divResult = result / i;
+            divRemainder = result % i;
+
+            multiply1 = divResult * n;
+            multiply2 = divRemainder * n;
+
+            result = multiply1 + (multiply2 / i);
+
+            n--;
         }
         return result;
-      }
+    }
 }
